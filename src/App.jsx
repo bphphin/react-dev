e1fb850d6c342932ddbdd721269fa9358ff1dd35
bphphin <buingocphinn@gmail.com>
2023-08-30 03:24:45 +0700

    refactor: :pencil2: format and rule of eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+
 const App = () => {
+    const [count, setCount] = useState(0);
     return <div>
         Home Page
+        <p>{ count }</p>
+        <button onClick={ () => setCount(count => count + 1) }>Dirty me</button>
     </div>
 }
 
