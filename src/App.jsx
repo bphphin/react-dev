994ac34c55172f0756475c6732bac80d0f37c2fc
bphphin <buingocphinn@gmail.com>
2023-09-05 01:34:06 +0700

    fix: :fire: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import Header from "./components/partials/Header";
 import Footer from "./components/partials/Footer";
-import MainContent from './components/MainContent';
 
 const App = () => {
-    const img = 'https://picsum.photos/200/200';
-    const img2 = 'https://picsum.photos/200/200';
-    const title = 'Đây là bài viết 1';
-    const title2 = 'Đây là bài viết 2';
-    const paragraph = 'đây là mô tả bài viết 1';
-    const paragraph2 = 'đây là mô tả bài viết 2';
     return <div>
         <Header/>
-        <MainContent img={ img } title={ title } paragraph={ paragraph }/>
-        <MainContent img={ img2 } title={ title2 } paragraph={ paragraph2 }/>
+        <p>Home Page</p>
         <Footer/>
     </div>
 }
