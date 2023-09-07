c98a3f417b7751b9f433364303f256bf1f1ac9aa
bphphin <buingocphinn@gmail.com>
2023-09-08 00:31:54 +0700

    fix: :fire: update component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,65 @@
-import React from "react";
-import ListUser from "./components/ListUser";
-
+import React, { useState } from "react";
+import Form from "./components/Form";
+import Table from "./components/Table";
 function App() {
-    return <ListUser/>
+    const [formData, setFormData] = useState({
+        id: null,
+        name: "",
+        address: "",
+        date: "",
+        conduct: "",
+    });
+    const [users, setUsers] = useState([]);
+    const handleFormData = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = () => {
+        if (formData.id) {
+            const newUser = users.map((item) =>
+                item.id === formData.id ? formData : item
+            );
+            setUsers(newUser);
+        } else {
+            formData.id = Date.now();
+            setUsers([...users, formData]);
+        }
+        setFormData({
+            id: null,
+            name: "",
+            address: "",
+            date: "",
+            conduct: "",
+        });
+    };
+
+    const handleDelete = (id) => {
+        const newUsers = users.filter((item) => item.id !== id);
+        setUsers(newUsers);
+    };
+
+    const handleUpdate = (user) => {
+        setFormData(user);
+    };
+
+    return (
+        <>
+            <Form
+                formData={formData}
+                handleSubmit={handleSubmit}
+                handleFormData={handleFormData}
+            />
+            <Table
+                data={users}
+                handleDelete={handleDelete}
+                handleUpdate={handleUpdate}
+            />
+        </>
+    );
 }
 
-export default App
+export default App;
